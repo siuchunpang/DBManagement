17a9ca219e5cb7b4d8882c836ec361d6e58f2ba7
Junpeng <623968625@qq.com>
2019-08-08 00:51:25 +0800

    login功能重构得很迷茫

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -5,32 +5,21 @@
         <div>数据库管理系统</div>
         <div class="middle-subtitle">欢迎登录</div>
       </div>
-      <div class="form">
+      <div class="form" v-for="(item, i) in arr" :key="i">
         <div class="el-input">
           <input
-            id="user"
-            type="text"
-            placeholder="用户名"
+            :id="item.id"
+            :type="item.type"
+            :placeholder="item.placeholder"
             class="el-input-inner"
-            :style="{'border-color': userBorderColor}"
-            @blur="handleUserBlur(user)"
-            v-model="user"
+            :style="{'border-color': item.borderColor}"
+            @blur="handleBlur(item.inputValue)"
+            v-model="item.inputValue"
           />
-          <span class="error" v-show="userError">用户名不能为空</span>
+          <span class="error" v-show="item.showError">用户名不能为空</span>
         </div>
-        <div class="el-input">
-          <input
-            id="password"
-            type="password"
-            placeholder="密码"
-            class="el-input-inner"
-            :style="{'border-color': passwordBorderColor}"
-            @blur="handlePasswordBlur(password)"
-            v-model="password"
-          />
-          <span class="error" v-show="passwordError">请输入密码</span>
-        </div>
-        <div class="el-form">
+      </div>
+      <div class="el-form">
           <div class="el-form-item">
             <button
               id="el-login"
@@ -39,7 +28,6 @@
             >
             登录</button>
           </div>
-        </div>
       </div>
       <div class="el-form el-show">
         <div class="el-form-span">
@@ -52,39 +40,46 @@
 </template>
 
 <script>
+import axois from 'axois'
 export default {
   name: "Login",
   data() {
     return {
-      user: '',
-      password: '',
-      userError: false,
-      passwordError: false,
-      userBorderColor: '#bbb',
-      passwordBorderColor: '#bbb'
+      arr: [{
+        id: 'user',
+        type: 'text',
+        placeholder: '用户名',
+        inputValue: '',
+        borderColor: '#bbb',
+        showError: false,
+        errorMsg: '用户名不能为空',
+      },{
+        id: 'password',
+        type: 'password',
+        placeholder: '密码',
+        inputValue: '',
+        borderColor: '#bbb',
+        showError: false,
+        errorMsg: '请输入密码',
+      }]
     }
   },
   methods: {
-    handleUserBlur(val) {
+    handleBlur(val) {
       if(!val) {
-        this.userError = true
-        this.userBorderColor = '#ff4040'
+        this.showError = true
+        this.borderColor = '#ff4040'
       } else {
-        this.userError = false
-        this.userBorderColor = '#bbb'
-      }
-    },
-    handlePasswordBlur(val) {
-      if(!val) {
-        this.passwordError = true
-        this.passwordBorderColor = '#ff4040'
-      } else {
-        this.passwordError = false
-        this.passwordBorderColor = '#bbb'
+        this.showError = false
+        this.borderColor = '#bbb'
       }
     },
     handleLoginClick() {
-
+      axois.post(url, {
+        params: {
+          
+        }
+      })
     }
   }
 }
